96193c6434a8e6db0ffa47b799e94f58a821c00a
hafsachenni <hafsachenni77@gmail.com>
2023-10-03 10:16:43 -0700

    combo of brains

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,5 @@
 for a in range(10):
     for h in range(a, 10):
         if a < h:
-            print("{:d}{:d}".format(a, h), end="\n" if a == 8 and h == 9 else ", ")
+            print("{:d}{:d}".format(a, h), 
+                    end="\n" if a == 8 and h == 9 else ", ")
