b37abdbbc710a78a0c98e75bea5bd74373d5aaa5
hafsachenni <hafsachenni77@gmail.com>
2023-10-03 11:35:27 -0700

    2 digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,4 @@ for a in range(10):
     for h in range(a, 10):
         if a < h:
             print("{:d}{:d}".format(a, h),
-                    end="\n" if a == 8 and h == 9 else ", ")
+                    end='\n' if a == 8 and h == 9 else ", ")
