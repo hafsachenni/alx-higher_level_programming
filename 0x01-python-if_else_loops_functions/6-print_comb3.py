5e593baa6e4ae696352f470529f16122af632039
hafsachenni <hafsachenni77@gmail.com>
2023-10-03 13:19:19 -0700

    power

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-for a in range(10):
+for a in range(0, 10):
+    if a == 8:
+        break
     for h in range(a, 10):
-        if a < h:
-            print('{}{}'.format(a, h), end=", ")
-            print('{}{}'.format(a, h))
+        if h == a:
+            continue
+        print('{}{}'.format(a, h), end=", ")
+    print('{}{}'.format(a, h))
