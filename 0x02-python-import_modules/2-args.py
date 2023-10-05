411c0b576502955ffe437cf3818ee4e285c9184a
hafsachenni <hafsachenni77@gmail.com>
2023-10-05 07:33:21 -0700

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if counter == 0:
         print("0 arguments.")
     elif counter == 1:
-        print("1 argument: ".format(sys.argv[1]))
+        print("1 argument:")
     else:
         print("{} arguments:".format(counter))
     for a in range(1, counter + 1):
