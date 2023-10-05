ffb455c3769a8db4cc129803b742e3895e231cc8
hafsachenni <hafsachenni77@gmail.com>
2023-10-05 09:50:22 -0700

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
 
-    module_names = dir(hidden_4)
-    print("Names defined here are:")
-    print(module_names)
+    for module_names in dir(hidden_4):
+        if module_names[:2] != "__":
+             print(module_names)
