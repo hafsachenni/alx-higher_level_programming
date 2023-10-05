1306688cb24c8251a7c301fdaf08708ce64b4dd1
hafsachenni <hafsachenni77@gmail.com>
2023-10-05 07:12:01 -0700

    arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    counter = len(sys.argv) -1
+    counter = len(sys.argv) - 1
 
     if counter == 0:
         print("0 arguments.")
