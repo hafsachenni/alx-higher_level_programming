37ee9d9c0a8cfa3c9c683c450d810748d7741a33
hafsachenni <hafsachenni77@gmail.com>
2023-10-06 10:58:38 -0700

    later

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    result = ' '
+    result = ''
     for char in my_string:
         if char != 'c' and char != 'C':
             result += char
---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for line in matrix:
+        for num in line:
+            print("{:d}".format(num),end=" ")
+        print()
