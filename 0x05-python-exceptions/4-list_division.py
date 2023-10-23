1eb2df83891eb976177d00a9cf31df1e8620f052
hafsachenni <hafsachenni77@gmail.com>
2023-10-23 11:47:07 -0700

    couldve done it earlier

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
+    
     for i in range(list_length):
         try:
-            list1 = my_list_1[i] if  i < len(my_list_1) else 1
-            list2 = my_list_2[i] if i < len(my_list_2) and my_list_2[i] != 0 else 1
+            list1 = my_list_1[i] 
+            list2 = my_list_2[i]
             result = list1 / list2
         except TypeError:
             print("wrong type")
