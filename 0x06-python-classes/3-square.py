0819b916ff00b300781bf3588f506a0c3f711538
hafsachenni <hafsachenni77@gmail.com>
2023-10-24 10:45:05 -0700

    area of square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''class square'''
+
+
+class Square:
+    '''initializes square with specified size'''
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+         self._area = self.__size * self.__size
+         return self._area
