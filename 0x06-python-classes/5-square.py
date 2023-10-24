de7899a2e11bf9836825dd4908ef86524ee6cdc2
hafsachenni <hafsachenni77@gmail.com>
2023-10-24 12:20:49 -0700

    hash

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''class square'''
+
+
+class Square:
+    '''initializes square with specified size'''
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, new_value):
+        if not isinstance(new_value, int):
+            raise TypeError("size must be an integer")
+        if new_value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = new_value
+
+    def area(self):
+        _area = self.__size * self.__size
+        return _area
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
