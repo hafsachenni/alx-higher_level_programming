942aaf1c090659ca9c825340ee16e1da2caf83b6
hafsachenni <hafsachenni77@gmail.com>
2023-10-25 14:25:38 -0700

    exception

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,5 +5,5 @@ def safe_function(fct, *args):
         put = fct(*args)
         return put
     except Exception as err:
-        print("Exception: {}".format(0), file=sys.stderr)
+        print("Exception: {}".format(err), file=sys.stderr)
         return None
