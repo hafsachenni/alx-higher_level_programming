607215a5e29127a79e8f6be1315d8bb09515c29d
hafsachenni <hafsachenni77@gmail.com>
2023-10-29 14:01:14 -0700

    i think we are good

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -8,6 +8,8 @@ class MyTestCase(unittest.TestCase):
     def test_max(self):
         self.assertEqual(max_integer([8, 3, 9, 7]), 9)
         self.assertEqual(max_integer([56]), 56)
+        self.assertEqual(max_integer([5, -8, 10]), 10)
+        self.assertEqual(max_integer([5.67, 5.66, 5.65]), 5.67)
 
     def test_value(self):
         self.assertRaises(TypeError, max_integer(), [5, "hi", 0])
