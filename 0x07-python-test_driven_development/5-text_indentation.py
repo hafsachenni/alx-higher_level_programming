bdd40b0bef4c45b3ef10256423d78a23d2e9c895
hafsachenni <hafsachenni77@gmail.com>
2023-10-29 06:52:27 -0700

    SAY BEYONNNNNCE

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    for a in range(len(text)):
+        if text[a] in [".", "?", ":"]:
+            print("{}\n".format(text[a]))
+        elif text[a] == " " and text[a - 1] in [".", "?", ":"]:    
+            continue
+        else:
+            print("{}".format(text[a]), end="")
+
+
---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,5 @@
+import unittest
+
+class MyTestCase(unittest.TestCase):
+    def max_integer(list=[]):
+
