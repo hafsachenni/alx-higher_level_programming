979f0c8e14352761689d842b3133e826ffa33de5
hafsachenni <hafsachenni77@gmail.com>
2023-10-31 08:49:37 -0700

    SAY BEYOOOOONCE

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""a function that prints the full name
+first_name and last_name must be strings
+otherwise raise typeerror"""
+
+
 def say_my_name(first_name, last_name=""):
+    """will print My name is <first name> <last name>
+    while handling errors"""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     elif not isinstance(last_name, str):
