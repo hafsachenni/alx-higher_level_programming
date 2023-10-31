df93c9a48053bc74301ed87c09e11d953877c149
hafsachenni <hafsachenni77@gmail.com>
2023-10-31 08:36:26 -0700

    adds

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
+"""function that adds 2 ints
+a and b must be integers otherwise raise TypeError
+if one of them is float cast it to an int
+b is equal to 98 by default
+return the sum"""
+
+
 def add_integer(a, b=98):
+    """casts a and b t ints if they are floats
+    if a or b are not integers then raise
+    typeerror, and return the addition"""
 
     if isinstance(a, float):
         a = int(a)
     if isinstance(b, float):
         b = int(b)
-        
     if not isinstance(a, int):
         raise TypeError("a must be an integer")
     if not isinstance(b, int):
