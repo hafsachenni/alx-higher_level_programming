2dda9db040e166df140d77a1b28060970551509a
hafsachenni <hafsachenni77@gmail.com>
2023-11-06 08:03:05 -0800

    stoooooop

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+'''define function'''
+
+
 def is_same_class(obj, a_class):
+    '''
+    returns True if the object is an instance of secified class
+    return true or false
+    '''
     if type(obj) == a_class:
         return True
     return False
