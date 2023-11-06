fb2f3cafca89112bce784f564eb70ca71e7b29d5
hafsachenni <hafsachenni77@gmail.com>
2023-11-06 09:42:42 -0800

    m tired

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''
+rectangle module
+'''
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    '''
+    rectangle
+    '''
+    def __init__(self, width, height):
+        '''
+        idth and height must be positive integers
+        '''
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def area(self):
+        '''
+        returs rectangles area
+        '''
+        return self.__width * self.__height
+
+    def __str__(self):
+        '''
+        rectangle
+        '''
+
+
