531a215e76103781c4ef7a94299782fa57927ffd
hafsachenni <hafsachenni77@gmail.com>
2023-11-07 06:44:02 -0800

    readble file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+defines a fucntion that reads  txt file
+'''
+
+
+def read_file(filename=""):
+    '''reads a txt file and prints it'''
+
+    with open(filename, 'r', encoding="utf-8") as file:
+        print(file.read())
