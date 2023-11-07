2a45938f3d113eb27cd5e8f7b823f387856ed7fd
hafsachenni <hafsachenni77@gmail.com>
2023-11-07 07:52:15 -0800

    json derulo

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -2,10 +2,9 @@
 '''
 defines a func that returns the json representation of an obj
 '''
-
-
 import json
+
+
 def to_json_string(my_obj):
     '''returns the representation'''
     return json.dumps(my_obj)
-
