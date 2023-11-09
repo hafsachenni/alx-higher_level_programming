a094c5532094de8078f7527618782f912ac77ecd
hafsachenni <hafsachenni77@gmail.com>
2023-11-09 13:13:07 -0800

    some included some excluded

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,8 +6,8 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         self.__width = width
         self.__height = height
-        self.x = x
-        self.y = y
+        self.__x = x
+        self.__y = y
         super().__init__(id)
 
     @property
