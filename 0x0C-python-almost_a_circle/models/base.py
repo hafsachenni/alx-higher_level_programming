869353ae0e8868d8a6e3ce3ed219d566889638c1
hafsachenni <hafsachenni77@gmail.com>
2023-11-10 13:21:16 -0800

    loads

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,7 +17,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         if list_dictionaries == None:
-            return []
+            return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
@@ -28,4 +28,17 @@ class Base:
         with open(filename, 'w', encoding="utf-8") as file:
             json_string = cls.to_json_string([obj.to_dictionary() for obj in list_objs])
             file.write(json_string)
+
+
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None:
+            return []
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+
 
