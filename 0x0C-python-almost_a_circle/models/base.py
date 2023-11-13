34ab3a4c3e528f5111b523f89f859e072a613f73
hafsachenni <hafsachenni77@gmail.com>
2023-11-13 05:34:07 -0800

    pycode + doc

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+'''base class of all child classes'''
 
 import json
 
-class Base:
 
+class Base:
     __nb_objects = 0
 
+    '''class atrributes'''
     def __init__(self, id=None):
         if id is not None:
             self.id = id
@@ -13,24 +15,24 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
-
+    '''returns the JSON string representation of list_dictionari     es'''
     @staticmethod
     def to_json_string(list_dictionaries):
-        if list_dictionaries == None:
+        if not list_dictionaries:
             return "[]"
         return json.dumps(list_dictionaries)
 
+    '''method that writes the json representation to a file'''
     @classmethod
     def save_to_file(cls, list_objs):
-        if list_objs == None:
+        if not list_objs:
             list_objs = []
         filename = cls.__name__ + ".json"
         with open(filename, 'w', encoding="utf-8") as file:
             json_string = cls.to_json_string([obj.to_dictionary() for obj in list_objs])
             file.write(json_string)
 
-
-
+    '''method that returns the list of json representation'''
     @staticmethod
     def from_json_string(json_string):
         if json_string is None:
@@ -38,13 +40,14 @@ class Base:
         else:
             return json.loads(json_string)
 
+    '''returns an instance with all atrributes'''
     @classmethod
     def create(cls, **dictionary):
         dummy_1 = cls(1, 1)
         dummy_1.update(**dictionary)
         return dummy_1
 
-
+    '''returns a list of all instances'''
     @classmethod
     def load_from_file(cls):
         filename = cls.__name__ + ".json"
@@ -52,9 +55,7 @@ class Base:
             with open(filename, "r", encoding="utf-8") as file:
                 json_string = file.read()
                 list_dict = cls.from_json_string(json_string)
-                instance = [cls.create(**o) for o in list_dict] 
+                instance = [cls.create(**o) for o in list_dict]
                 return instance
         except FileNotFoundError:
             raise FileNotFoundError("file not found")
-
-
