c8f6f52c1ed8686439e41d50c3ba052eb25b120b
hafsachenni <hafsachenni77@gmail.com>
2023-11-13 07:50:09 -0800

    pycode cape on and off

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,86 +6,87 @@ from models.base import Base
 class Rectangle(Base):
     '''defining class attributes'''
     def __init__(self, width, height, x=0, y=0, id=None):
+        '''class constructor'''
         self.width = width
         self.height = height
         self.x = x
         self.y = y
         super().__init__(id)
 
-    '''width attr getter'''
     @property
     def width(self):
+        '''width attr getter'''
         return self.__width
 
-    '''width atrr setter'''
     @width.setter
     def width(self, value):
+        '''width atrr setter'''
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
 
-    '''height attr getter'''
     @property
     def height(self):
+        '''height attr getter'''
         return self.__height
 
-    '''height attr setter'''
     @height.setter
     def height(self, value):
+        '''height attr setter'''
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
 
-    '''x attr getter'''
     @property
     def x(self):
+        '''x attr getter'''
         return self.__x
 
-    '''x attr setter'''
     @x.setter
     def x(self, value):
+        '''x attr setter'''
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
 
-    '''y attr getter'''
     @property
     def y(self):
+        '''y attr getter'''
         return self.__y
 
-    '''y attr setter'''
     @y.setter
     def y(self, value):
+         '''y attr setter'''
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
 
-    '''method that returns the area of the rectangle instance'''
     def area(self):
+         '''method that returns the area of the rectangle instance'''
         return self.__width * self.__height
 
-    '''method that prints in stdout the rectangle with # char'''
     def display(self):
+        '''method that prints in stdout the rectangle with # char'''
         for a in range(self.y):
             print()
         for i in range(self.height):
             print(" " * self.x + "#" * self.width)
 
-    '''method that prints the str representation of the rectangle'''
     def __str__(self):
+        '''method that prints the str representation'''
         return "[Rectangle] ({}) {}/{} - {}/{}"\
                 .format(self.id, self.x, self.y, self.width, self.height)
 
-    '''method that assigns a key/value args to attr'''
     def update(self, *args, **kwargs):
+        '''method that assigns a key/value args to attr'''
         if args:
             attr = ["id", "width", "height", "x", "y"]
             for i, value in enumerate(args):
@@ -94,8 +95,8 @@ class Rectangle(Base):
                 for key, value in kwargs.items():
                     setattr(self, key, value)
 
-    '''returns the dictionary represenation of the rectangle'''
     def to_dictionary(self):
+        '''returns the dictionary represenation of the rectangle'''
         return {
             'x': self.__x,
             'y': self.__y,
