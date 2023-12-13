c8a17d925e6d250d8158f17bd866d2432c786e6a
hafsachenni <hafsachenni77@gmail.com>
2023-12-13 14:55:52 -0800

    all done

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,8 @@
+-- script that uses the hbtn_0d_tvshows database
+SELECT tv_genres.name FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows
+ON tv_showd.id = tv_show_genres.show_id
+WHERE tv_shows.title = "DEXTER"
+ORDER BY tv_genres.name;
