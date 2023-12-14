faaf8991b78457e7f0b2eceb045044f40ef8eea8
hafsachenni <hafsachenni77@gmail.com>
2023-12-14 00:51:32 -0800

    lets see

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,6 +1,6 @@
 --  script that lists all Comedy shows in the database hbtn_0d_tvshows
 SELECT tv_shows.title FROM tv_shows
-INNER JOIN tv_shows_genres
+INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 INNER JOIN tv_genres
 ON tv_genres.id = tv_show_genres.genre_id
