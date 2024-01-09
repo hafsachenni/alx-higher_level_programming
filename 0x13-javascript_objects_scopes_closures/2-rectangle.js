04212ad23c359dea7a402adcfdee213f5470364f
hafsachenni <hafsachenni77@gmail.com>
2024-01-09 06:32:31 -0800

    dont do it again

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-	  if (w > 0 && h > 0) {
-		  this.width = w;
-		  this.height = h;
-	  } else if (w && h < 0) {
-		  Object.create(null);
-	  } else if (w && h === 0) {
-		  Object.create(null);
-	  }
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else if (w && h < 0) {
+      Object.create(null);
+    } else if (w && h === 0) {
+      Object.create(null);
+    }
   }
 };
