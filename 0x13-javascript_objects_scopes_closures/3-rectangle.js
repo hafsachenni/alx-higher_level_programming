27b0e4dd37ca590630433466b2593123f70f4e6e
hafsachenni <hafsachenni77@gmail.com>
2024-01-09 06:53:04 -0800

    maytrid

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else if (w && h < 0) {
+      Object.create(null);
+    } else if (w && h === 0) {
+      Object.create(null);
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let x = '';
+      for (let a = 0; a < this.width; a++) {
+        x += 'X';
+      }
+      console.log(x);
+    }
+  }
+};
