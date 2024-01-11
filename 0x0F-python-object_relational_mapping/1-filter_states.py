d95b259a9b2ec8fd112400eb8be4def55b08d266
hafsachenni <hafsachenni77@gmail.com>
2024-01-11 09:23:51 -0800

    got jouj jouj mn lhaja

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+script that lists all states with a name starting with N
+upper N) from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    mydb = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            password=sys.argv[2],
+            database=sys.argv[3]
+            )
+
+    mycursor = mydb.cursor()
+    query = "SELECT *FROM states WHERE name LIKE 'N%'"
+    mycursor.execute(query)
+    myresult = mycursor.fetchall()
+
+    for result in myresult:
+        print(result)
