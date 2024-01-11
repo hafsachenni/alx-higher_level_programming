c686af8f508550e8ed034ae8096d57f0d114da4d
hafsachenni <hafsachenni77@gmail.com>
2024-01-11 09:43:32 -0800

    daba wallaw 3, 3 arizonas

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+script that takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    mydb = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            password=sys.argv[2],
+            database=sys.argv[3]
+            )
+
+    mycursor = mydb.cursor()
+    query = "SELECT *FROM states WHERE name = '{}'".format(sys.argv[4])
+    mycursor.execute(query)
+    myresult = mycursor.fetchall()
+
+    for result in myresult:
+        print(result)
