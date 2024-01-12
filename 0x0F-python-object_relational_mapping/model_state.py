864dea80abcba29980d9573363c889662b2fd288
hafsachenni <hafsachenni77@gmail.com>
2024-01-12 06:39:37 -0800

    defined state table

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,8 +3,6 @@
 python file that contains the class definition of a State
 and an instance Base = declarative_base()
 """
-
-
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -13,6 +11,14 @@ Base = declarative_base()
 
 
 class State(Base):
+    """
+    creates state class
+    links to the MySQL table states
+    class attribute id that represents a column
+    class attribute name that represents a column
+     must use the module SQLAlchemy
+     should connect to a MySQL server running on localhost at port 3306
+     """
 
     __tablename__ = "states"
     id = Column(Integer, primary_key=True, nullable=False)
