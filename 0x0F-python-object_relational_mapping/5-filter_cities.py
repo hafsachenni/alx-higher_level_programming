fdfd050efeb7a8f6d43c276827176eba77bb6e4d
hafsachenni <hafsachenni77@gmail.com>
2024-01-12 04:28:45 -0800

    texas and hawaii

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+cript that lists all cities from the database
+hbtn_0e_4_usa
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    mydb = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            password=sys.argv[2],
+            database=sys.argv[3]
+            )
+
+    mycursor = mydb.cursor()
+    query = """SELECT GROUP_CONCAT(cities.name
+    ORDER BY cities.id ASC SEPARATOR ', ')
+    FROM cities
+    JOIN states ON cities.state_id = states.id
+    WHERE states.name = %s;
+    """
+    mycursor.execute(query, (sys.argv[4],))
+    myresult = mycursor.fetchall()
+
+    for result in myresult:
+        print(result[0])
