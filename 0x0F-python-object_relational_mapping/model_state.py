21eb6a73f80ffaedc0dd09e1bb8f0311ff69576a
hafsachenni <hafsachenni77@gmail.com>
2024-01-12 08:03:56 -0800

    a little test wont hurt

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -23,3 +23,5 @@ class State(Base):
     __tablename__ = "states"
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
+
+Base.metadata.create.all(engine)    
