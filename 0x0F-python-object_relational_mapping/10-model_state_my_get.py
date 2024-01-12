96e72a895822c7e7ba7a644c6bfacae47b18728a
hafsachenni <hafsachenni77@gmail.com>
2024-01-12 12:49:27 -0800

    khdaaam

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
 """
-Define a State class
-that inherits from sqlalchemy Base
+lists all State objects from the database
+hbtn_0e_6_usa
 """
 from sqlalchemy import create_engine
+import sys
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
-import sys
+
 
 if __name__ == "__main__":
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'
-        .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-
-    # A session is like a workspace for making changes and
-    # interacting with the database.
+            'mysql+mysqldb://{}:{}@localhost/{}'
+            .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state_names = session.query(State).filter(State.name.like(sys.argv[4]))
+    natijat = session.query(State).filter(State.name.like(sys.argv[4]))
 
-    if not state_names.all():
-        print("Not found")
+    if not natijat.all():
+        print("Not Found")
     else:
-        for state in state_names:
-            print('{}'.format(state.id))
+        for natija in natijat:
+            print('{}'.format(natija.id))
