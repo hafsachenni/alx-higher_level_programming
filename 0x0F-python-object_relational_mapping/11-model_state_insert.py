9d06e517a7449f3a30d28a40f7948cfddf7c636d
hafsachenni <hafsachenni77@gmail.com>
2024-01-12 13:23:00 -0800

    zid luoisian

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+lists all State objects from the database
+hbtn_0e_6_usa
+"""
+from sqlalchemy import create_engine
+import sys
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'
+            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    natijat = State(name = "Louisiana")
+    session.add(natijat)
+    session.commit()
+    print(natijat.id)
