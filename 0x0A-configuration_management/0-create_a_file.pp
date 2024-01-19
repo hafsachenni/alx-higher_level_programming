3118832a6c4889f35d1b441b7aac1bd1366eecaa
hafsachenni <hafsachenni77@gmail.com>
2024-01-19 06:28:22 -0800

    fin tban betty

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Using Puppet, i'am creating a file in /tmp
+
+file {'/tmp/school':
+	ensure => present,
+	mode => '0744',
+	owner => 'www-data',
+	group => 'www-data',
+	content => 'I love Puppet',
+}
