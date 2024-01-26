23b4c6155e3df5493ddf9937052ea9eb1229ffe1
hafsachenni <hafsachenni77@gmail.com>
2024-01-26 09:47:47 -0800

    header value displayed binajah

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""send http request n display value of variable X-Request-Id"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    response = requests.get(sys.argv[1])
+    alo = response.headers["X-Request-Id"]
+    print(alo)
