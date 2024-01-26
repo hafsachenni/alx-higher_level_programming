be95603ad9a0067c9de29fa8ad2ebad6e2a71a52
hafsa chenni <hafsachenni77@gmail.com>
2024-01-26 21:03:51 +0100

    mchat

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,4 +5,9 @@ import sys
 
 
 if __name__ == "__main__":
-    email = {'
+    email = sys.argv[2]
+    payload = {'email': email}
+    response = requests.post(sys.argv[1], data=payload)
+
+    print("Your email is:", email)
+    print(response.text)
