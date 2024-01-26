cd85ba49c8e0c31edb88870e9cda21be32655b9b
hafsachenni <hafsachenni77@gmail.com>
2024-01-26 06:56:56 -0800

    response header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,4 +5,7 @@ retuns value of variable found in the header of the response
 import urllib.request
 
 
-
+if __name__ == "__main__":
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io") as res:
+        alo = res.getheader("X-Request-Id")
+        print(alo)
