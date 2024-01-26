647860c91b5f4cad1a1d655b33637518fee9f390
hafsachenni <hafsachenni77@gmail.com>
2024-01-26 12:32:13 -0800

    chaditak aawtani

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""bash script that dispalys body of response"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    try:
+        response = requests.get(sys.argv[1])
+        response.raise_for_status()
+        print(response.text)
+    except requests.exceptions.HTTPError as chaditak:
+        print("Error code:", chaditak.status_code)
