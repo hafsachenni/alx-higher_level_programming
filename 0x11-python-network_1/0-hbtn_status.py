beae550470971d17731dcd7eb2a4cf25a1e082b1
hafsachenni <hafsachenni77@gmail.com>
2024-01-26 06:45:37 -0800

    http request sent

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,9 +4,9 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as res:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         me = res.read()
-    print('Body Response:')
+    print("Body response:")
     print("\t- type: {}".format(type(me)))
-    print('\t- content: {}'.format(me))
-    print('\t- utf8 content: {}'.format(me.decode("utf-8")))
+    print("\t- content: {}".format(me))
+    print("\t- utf8 content: {}".format(me.decode("utf-8")))
