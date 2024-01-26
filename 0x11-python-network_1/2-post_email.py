d280a7a22df3480770dff6cbd0bc4ae3307a4de1
hafsachenni <hafsachenni77@gmail.com>
2024-01-26 08:06:02 -0800

    post request sent mb

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,12 +4,14 @@ import urllib.request
 import sys
 import urllib.parse
 
+
 if __name__ == "__main__":
     url = sys.argv[1]
     value = {'email': sys.argv[2]}
     """encoding the data"""
     data = urllib.parse.urlencode(value).encode("utf-8")
+    """request object creation"""
     request = urllib.request.Request(url, data=data, method='POST')
     with urllib.request.urlopen(request) as res:
-        alo = res.read())
-        print(alo.decode("utf-8")
+        alo = res.read()
+        print(alo.decode("utf-8"))
