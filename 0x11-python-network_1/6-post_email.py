e46c2bf78c26a4a8c8a9e8aff6ba5b2878b3173d
hafsachenni <hafsachenni77@gmail.com>
2024-01-26 12:38:00 -0800

    post request

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,7 +7,5 @@ import sys
 if __name__ == "__main__":
     email = sys.argv[2]
     payload = {'email': email}
-    response = requests.post(sys.argv[1], data=payload)
-
-    print("Your email is:", email)
+    response = requests.post(sys.argv[1], payload)
     print(response.text)
