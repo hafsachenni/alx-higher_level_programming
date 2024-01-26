8aa527f67757d32d01ff3f74f860c9be9d2d0bd6
hafsachenni <hafsachenni77@gmail.com>
2024-01-26 06:40:35 -0800

    http request sent successfully

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""
-python script that fetches https://alx-intranet.hbtn.io/status
-"""
+"""python script that fetches url"""
 import urllib.request
 
-
 if __name__ == "__main__":
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
-        me = response.read()
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as res:
+        me = res.read()
     print('Body Response:')
     print("\t- type: {}".format(type(me)))
     print('\t- content: {}'.format(me))
---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+retuns value of variable found in the header of the response
+"""
+import urllib.request
+
+
+
