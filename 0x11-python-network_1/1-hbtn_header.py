2eac361aeee900da94e98023dd11ea553855696e
hafsachenni <hafsachenni77@gmail.com>
2024-01-26 07:00:17 -0800

    http response header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,5 +7,6 @@ import urllib.request
 
 if __name__ == "__main__":
     with urllib.request.urlopen("https://alx-intranet.hbtn.io") as res:
+        me = res.read()
         alo = res.getheader("X-Request-Id")
         print(alo)
