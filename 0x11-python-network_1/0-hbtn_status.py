3b42a748ebeb15a5a7c53ee48cd887e65f1e4d57
hafsachenni <hafsachenni77@gmail.com>
2024-01-26 06:26:20 -0800

    testin gwithiout "with"

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-
+"""
+python script that fetches https://alx-intranet.hbtn.io/status
+"""
 import urllib.request
+
+
 if __name__ == "__main__":
     response = urllib.request.urlopen("https://alx-intranet.hbtn.io/status")
     me = response.read()
-    print(me)
+    print('Body Response:')
+    print("\t- type: {}".format(type(me)))
+    print('\t- content: {}'.format(me))
+    print('\t- utf8 content: {}'.format(me.decode("utf-8")))
