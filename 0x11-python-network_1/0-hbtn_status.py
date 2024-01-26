6d413331b8a0c99560b06a4fffc1d973280eb242
hafsachenni <hafsachenni77@gmail.com>
2024-01-26 06:41:45 -0800

    http request sent successfully

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,6 +2,7 @@
 """python script that fetches url"""
 import urllib.request
 
+
 if __name__ == "__main__":
     with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as res:
         me = res.read()
