bca694ca83a2f9189ab77175cc6543b2045101c3
hafsachenni <hafsachenni77@gmail.com>
2024-01-26 13:27:56 -0800

    json format

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,17 +5,14 @@ import sys
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        q = ""
-    else:
-        q = sys.argv[1]
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
     value = {'q': letter}
-    response = requets.post(sys.argv[1], data=value)
+    response = requests.post("http://0.0.0.0:5000/search_user", data=value)
     try:
         j = response.json()
         if j == {}:
             print("No result")
         else:
-            print("[{}] {}".format(j.get(id), j.get(name)))
+            print("[{}] {}".format(j.get('id'), j.get('name')))
     except ValueError:
         print("Not a valid JSON")
