e805f43ca08a96f8c8d5253627ef1d5b255336be
hafsachenni <hafsachenni77@gmail.com>
2024-01-26 14:06:53 -0800

    authenyification

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""script that takes my github credentials"""
+import requests
+import sys
+import requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    douz = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    res = requests.get('https://api.github.com/user', auth=douz)
+    print(res.json().get("id")):
