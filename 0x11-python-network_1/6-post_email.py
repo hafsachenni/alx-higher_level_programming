3dd77c370e599e3d0f26e2a4b6a0906c17a23874
hafsachenni <hafsachenni77@gmail.com>
2024-01-29 04:30:09 -0800

    alo alo

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,4 +5,9 @@ import sys
 
 
 if __name__ == "__main__":
-    email = {'
+    email = {'alo': 'chkoun'}
+    response = requests.post(sys.argv[1], data=email)
+    alo = response.text
+    print(alo)
+    print("Your email is: ", email)
+
