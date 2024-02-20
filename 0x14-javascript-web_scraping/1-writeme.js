9c97d2ff4e539552fe2ff8873a543d794f6c85b5
hafsa chenni <hafsachenni77@gmail.com>
2024-02-20 17:02:31 +0100

    writing  a str to file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,6 +3,7 @@
 const pathTofile = process.argv[2];
 const stringTowrite = process.argv[3];
 const fs = require('fs');
-fs.writeFile(pathTofile, stringTowrite,'utf-8', function(error) {
-    if (error) console.error(error);
+fs.writeFile(pathTofile, stringTowrite, 'utf-8', function (error) {
+  // if an err occured during writing, ghowat
+  if (error) console.log(error);
 });
