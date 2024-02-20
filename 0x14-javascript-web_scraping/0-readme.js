bc16b3c7795c380dc36b8eb6f32fd7e6aefe1c2d
hafsa chenni <hafsachenni77@gmail.com>
2024-02-20 16:20:19 +0100

    reading from a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-// reading from a file that will be provided as cmd line arg
 const fs = require('fs');
-const file_path = process.argv[2];
-fs.readFile(file_path, 'utf8', function (error, data) {
-  console.log(error || data);
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
