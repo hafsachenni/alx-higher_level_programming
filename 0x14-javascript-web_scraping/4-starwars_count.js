cc982adfdcb45b76acc5213bad92e7603bda244e
hafsa chenni <hafsachenni77@gmail.com>
2024-02-20 20:00:54 +0100

    num of times wedges is present

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const apiUrl = process.argv[2];
+const charId = 18;
+
+const request = require('request');
+request.get(apiUrl, function (error, response, body) {
+  if (error) {
+    console.error('Error:', error);
+  } else {
+    const res = JSON.parse(body).results;
+
+    let count = 0;
+    for (const result of res) {
+      const filmschars = result.characters;
+
+      for (const film of filmschars) {
+        if (film.includes(`/${charId}/`)) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  }
+});
