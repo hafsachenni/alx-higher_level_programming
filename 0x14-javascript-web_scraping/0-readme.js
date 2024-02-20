77808ee42901f39eae4351bb9f9844b2bba26996
hafsa chenni <hafsachenni77@gmail.com>
2024-02-20 16:29:11 +0100

    reading file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
+const filepath = process.argv[2];
+fs.readFile(filepath, 'utf8', function (error, data) {
+  console.log(error || data);
 });
