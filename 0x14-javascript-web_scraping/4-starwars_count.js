55cde40565350dcf1a9275099c706a84ceb41d7b
hafsa chenni <hafsachenni77@gmail.com>
2024-02-20 20:12:13 +0100

    num of times wedges is present

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -8,13 +8,16 @@ request.get(apiUrl, function (error, response, body) {
   if (error) {
     console.error('Error:', error);
   } else {
+    // get results in json format
     const res = JSON.parse(body).results;
 
     let count = 0;
     for (const result of res) {
+        // loop aala results and fetch characters
       const filmschars = result.characters;
 
       for (const film of filmschars) {
+        // loop aala characters and get the one with the requetsed id
         if (film.includes(charId)) {
           count++;
         }
