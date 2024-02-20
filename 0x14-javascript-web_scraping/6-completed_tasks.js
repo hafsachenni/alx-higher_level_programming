7b179b31098b46fa5d77fe8333ffc75603cd0970
hafsa chenni <hafsachenni77@gmail.com>
2024-02-20 23:10:45 +0100

    tasks done by a user

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -15,6 +15,8 @@ request.get(url, function (error, response, body) {
   // iterating through each task
   for (const task of todos) {
     if (task.completed) {
+      // if count already exists it takes current count
+      // if not, it uses 0 as default value, and increments count by 1
       completedTasks[task.userId] = (completedTasks[task.userId] || 0) + 1;
     }
   }
