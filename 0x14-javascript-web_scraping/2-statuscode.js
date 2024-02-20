5a2aa6a84a906a5d4dcea128617e058c6f0f25ee
hafsa chenni <hafsachenni77@gmail.com>
2024-02-20 17:35:46 +0100

    status code returned

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-
-const request = require('request');
-
 // script that displays the status code of a GET request
+const request = require('request');
 const url = process.argv[2];
 request.get(url).on('response', function (response) {
   // response event represents the HTTP res obj returned by the server
