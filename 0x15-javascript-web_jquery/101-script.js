abe46fa64b18764619bfe774d310554d63894d1f
hafsa chenni <hafsachenni77@gmail.com>
2024-02-28 18:31:50 +0100

    add/remove/clear

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -1,11 +1,14 @@
-$('#add_item').click(function() {
-    const newitem = $("<li>Item</li>");
-    $('UL.my_list').append(newitem);
-});
-$('#remove_item').click(function() {
-    $("UL.my_list li:last-child").remove();
-});
-
-$('#clear_list').click(function() {
-    $("UL.my_list").empty();
-});
+$(document).ready(function (){
+    $('#add_item').click(function() {
+        const newitem = $("<li>Item</li>");
+        $('UL.my_list').append(newitem);
+    });
+    $('#remove_item').click(function() {
+        $("UL.my_list li:last-child").remove();
+    });
+    
+    $('#clear_list').click(function() {
+        $("UL.my_list").empty();
+    });
+    
+})
