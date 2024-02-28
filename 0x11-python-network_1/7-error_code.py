71748b5addffaa601a7b90a59aeb016022080dd2
hafsa chenni <hafsachenni77@gmail.com>
2024-02-28 17:56:21 +0100

    ajax

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -11,3 +11,4 @@ if __name__ == "__main__":
         print(response.text)
     except requests.exceptions.HTTPError as chaditak:
         print("Error code:", chaditak.response.status_code)
+        
